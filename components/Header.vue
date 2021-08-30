6093a3aae2191152915022650717918aeeb7f75a
strongenough07 <systembk8445@gmail.com>
2021-08-30 21:05:13 +0300

    Header and Connect Api

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="text-center bg-red-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">Covid19-Tracker</div>
+    <p>
+      API by
+      <a class="text-red-300" href="https://covid19api.com" target="_blank"
+        >covid19api</a
+      >
+    </p>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,38 @@
 <template>
-  <Tutorial/>
+  <div>
+    <main v-if="!loading">Show Data</main>
+    <main class="flex flex-col align-center justify-center text-center" v-else>
+      <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+      <img :src="loadingImage" class="w-24 m-auto" alt="" />
+    </main>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/images/hourglass.gif'),
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    this.dataDate = data.Date
+    this.stats = data.Globul
+    this.countries = data.Countries
+    this.loading = false
+  },
+}
 </script>
