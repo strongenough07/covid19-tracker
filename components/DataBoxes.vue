6d79476b7d278a8eace80d9860e87b072033204a
strongenough07 <systembk8445@gmail.com>
2021-08-30 21:53:55 +0300

    Created DataTitle and DataBoxes2

---
+++ components/DataBoxes.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="grid md:grid-cols-2gap-4">
+      <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+        <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+        <div class="text-2xl mb-4">
+          <span class="font-bold">New:</span>
+          {{ numberWithCommas(stats.NewConfirmed) }}
+        </div>
+        <div class="text-2xl mb-4">
+          <span class="font-bold">Total:</span>
+          {{ numberWithCommas(stats.TotalConfirmed) }}
+        </div>
+      </div>
+      <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+        <h3 class="text-3xl text-blue-900 font-bold mb-4">Deadths</h3>
+        <div class="text-2xl mb-4">
+          <span class="font-bold">New:</span>
+          {{ numberWithCommas(stats.NewDeaths) }}
+        </div>
+        <div class="text-2xl mb-4">
+          <span class="font-bold">Total:</span>
+          {{ numberWithCommas(stats.TotalDeaths) }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['stats'],
+  methods: {
+    numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ components/DataTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-center">
+    <h2 class="text-3xl font-bold">{{ text }}</h2>
+    <div class="text-2xl nt-4 mb-10">{{ timestamp }}</div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  props: ['text', 'dataDate'],
+  computed: {
+    timestamp: function () {
+      return moment(this.dataDate).format('MMMM Do YYYY, h:mm:ss a')
+    },
+  },
+}
+</script>
+
+<style></style>
